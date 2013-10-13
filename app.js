224c88fb836f6bac10c855cb6012862a9859d52e
Alexandr Dudar <alexandr.dudar@gmail.com>
2013-10-14 00:59:06 +0300

    graph
    graph example

---
+++ app.js
@@ -0,0 +1,33 @@
+function fruitCtrl($scope) {  
+	$scope.items = [
+		{
+			title : "Apple",
+			value : 45
+		},
+		{
+			title : "Orange",
+			value : 17
+		},
+		{
+			title : "banana",
+			value : 32
+		},
+	];
+}
+  
+function itemTableCtrl($scope) {                                 
+	$scope.add = function(){          
+		$scope.items.push({
+			value : 0
+		});                                 
+	};
+
+	$scope.maxValue = function(item){
+		if (item.value > 100){ 
+			item.value = 100;
+		}else if (item.value < 0){
+			item.value = 0
+		}
+	};
+}      
+    
